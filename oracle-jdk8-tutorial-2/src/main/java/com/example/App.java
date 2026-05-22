81fd9153c40bd71e3dd26667588f74147675cbc9
vrma <vrmachado@gmail.com>
2026-05-22 18:48:14 +0200

    antes de utilizar la biblioteca de lombok

--- oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
+++ oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
@@ -29,12 +29,42 @@ public class App {
     	 * Vamos a crear dos objetos Manzana que sean de la misma variedad y precio */
     	
     	Manzana manzana3 = new Manzana("reineta", new BigDecimal(2.0));
+    	
     	Manzana manzana4 = new Manzana("reineta", new BigDecimal(2.0));
     	
     	
+    	int x = 5;
+    	int y = 5;
+    	
+    	// Si quiero comprobar si las variables x e y son iguales, podemos utilizar una sentencia de Java
+    	// que la tienen todos los lenguajes de programacion, denominada sentencia condicional if - else
+    	// que evalua una condicion entre parentesis, y que si se cumple se ejecuta el grupo de sentencias 
+    	// que aparecen en el bloque encerrado entre las llaves de apartura y cierre, y si no se cumple la condicion
+    	// se podra ejecutar la o las sentencias que estan en siguiente bloque.
+    	
+    	if (x == y) {
+    		System.out.println("Las variables son iguales ");
+    	} else {
+    		System.out.println("El valor de las variables es diferente ");
+    	}
     	
     	
+    	if (manzana3 == manzana4) {
+    		System.out.println("Son iguales");
+    	} else {
+    		System.err.println("Son diferentes");
+    	}
     	
+    	/* Para saber si el contenido de dos objetos es el mismo se utiliza el metodo equals, porque el operador
+    	 * == lo que comprueba en el caso de objetos es si dichos objetos estan en la misma ubicacion de la memoria
+    	 * */
+    	
+    	if (manzana3.equals(manzana4)) {
+    		
+    		System.out.println("El contenido de ambos objetos es el mismo");
+    	} else {
+    		System.err.println("El contenido de ambos objetos es diferente");
+    	}
       	
     }
 }
