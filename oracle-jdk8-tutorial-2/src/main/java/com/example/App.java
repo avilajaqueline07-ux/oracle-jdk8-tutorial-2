2e0dfff1c5c256b70b78792becd200c535f38cf5
vrma <vrmachado@gmail.com>
2026-06-04 19:35:28 +0200

    sentencia do while explicada

--- oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
+++ oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
@@ -658,6 +658,23 @@ public class App {
 		
 		System.out.println("El valor final de z es : " + z);
 		
+		/* Sentencia do - while. 
+		 * 
+		 * La condicion se evalua al final, no al principio como
+		 * en la sentencia while, por lo que si se necesita ejecutar
+		 * el bloque de instrucciones que estan entre las llaves de
+		 * apertura y cierre, una vez al menos, pues esta es la sentencia
+		 * indicada */
+		
+		int zz = 20;
+		
+		do {
+			System.out.println("No se cumple ya la condicion y "
+					+ "hemos entrado al bucle una vez, de todas formas ");
+			zz -= 5;
+		} while (zz < 10);
+			
+		
 	}
 	
 	// Metodo de instancia, es decir, sin el modificador static,
